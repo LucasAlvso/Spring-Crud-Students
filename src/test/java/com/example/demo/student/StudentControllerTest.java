37815c57ce56e3f24de10a0c592cebad11efc9c2
Lucas Alves <lucas.soares008@edu.pucrs.br>
2023-02-18 22:04:36 -0300

    Implemented StudentService tests and deleted StudentControllerTest file.

--- src/test/java/com/example/demo/student/StudentControllerTest.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.student;
-
-public class StudentControllerTest
-{
-
-}
--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -1,14 +1,16 @@
 package com.example.demo.student;
 
-import com.example.demo.student.Student;
-import com.example.demo.student.StudentService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class StudentServiceTest
@@ -17,8 +19,11 @@ public class StudentServiceTest
     @Autowired
     private StudentService studentService;
 
+    @MockBean
+    private StudentRepository studentRepository;
+
     @Test
-    public void ShouldCheckIfModelIsInValid()
+    void ShouldCheckIfModelIsInValid()
     {
         Student student = new Student("", null, null);
 
@@ -26,10 +31,35 @@ public class StudentServiceTest
     }
 
     @Test
-    public void ShouldCheckIfModelIsValid()
+    void ShouldCheckIfModelIsValid()
     {
         Student student = new Student("Lucas", LocalDate.now(), "email@gmail.com");
 
         assertTrue(studentService.studentModelIsValid(student));
     }
+
+    @Test
+    void shouldFindStudentById()
+    {
+        Student student = new Student(90L, "Carlos", LocalDate.of(1999, 8, 25), "carlos@gmail.com");
+
+        when(studentRepository.findById(90L)).thenReturn(Optional.of(student));
+
+        Student result = studentService.findStudentById(90L).get();
+
+        assertEquals(student, result);
+    }
+
+    @Test
+    void shouldSaveStudent()
+    {
+        Student student = new Student(90L, "Carlos", LocalDate.of(1999, 8, 25), "carlos@gmail.com");
+
+        when(studentRepository.save(student)).thenReturn(student);
+
+        Student result = studentRepository.save(student);
+
+        assertEquals(student, result);
+    }
+
 }
